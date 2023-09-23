6b1ec5fb267704a6b2712de78d5c98793fc8b4e7
ManqobaGoodman <97664119+ManqobaGoodman@users.noreply.github.com>
2023-09-23 13:58:59 +0200

    LogIn and SiggIn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashbardComponent } from './components/dashbard/dashbard.component';
+import { AuthGuard } from './Guards/auth.guard';
+import { ResetComponent } from './components/reset/reset.component';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'signup', component:SignupComponent},
-  {path:'dashbard',component:DashbardComponent}
+  {path:'dashbard',component:DashbardComponent, canActivate:[AuthGuard]},
+  {path: 'reset', component:ResetComponent}
 ];
 
 @NgModule({
--- src/app/components/dashbard/dashbard.component.ts
+++ src/app/components/dashbard/dashbard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserStoreService } from 'src/app/services/user-store.service';
 
 @Component({
   selector: 'app-dashbard',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashbard.component.scss']
 })
 export class DashbardComponent implements OnInit {
-
-  constructor() { }
+  public users:any = [];
+  public fullName: string = "";
+  public role: string ="";
+  constructor(private authService: AuthService, private apiService: ApiService, private userStoreService: UserStoreService) { }
 
   ngOnInit(): void {
+    this.apiService.getUsers()
+    .subscribe(res=> {
+      this.users = res;
+    });
+
+    this.userStoreService.getFullNameFromStore()
+    .subscribe(val =>{
+      let fullNameToken = this.authService.getFullNameFromTokene();
+      this.fullName = val || fullNameToken;
+    });
+
+    this.userStoreService.getRoleFromStore()
+    .subscribe(val =>{
+      let roleToken = this.authService.getRoleFromToken();
+      this.role = val || roleToken;
+    });
+
+    console.log('Role: ' + this.role)
+  }
+
+  logout(){
+    this.authService.signout();
   }
 
 }
